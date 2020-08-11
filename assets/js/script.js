47f584c5dc83a7b93893302ffcf8a79f345e1a15
DougCBrown <98bluecamaro@gmail.com>
2020-08-11 11:09:09 -0400

    updating the structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,72 +1,28 @@
 
 //Buttons will save user inputs to local store. Additionally if there is saved inputs, they will display in the textarea.
+//Sets the current date at the top of the page.
+$(document).ready(function() {
+
+    $("#currentDate").text( moment().format('ddd MMM Do, YYYY'));
+
+});
+// here I am trying to build the function to fill the
+// background colors for the textarea future - green, current - red, past - gray
 function update() {
     var currentDate = new Date();
-    $(".time-block").each(function () {
-        var thisHours = $(this).data("hours");
-        var difference = currentDate.getHours() - thisHours;
 
-        if(difference < 0) {
-            $(this).find("textarea").addClass("future");
-        }else if(difference === 0) {
-            $(this).find("textarea").addClass("current");
-        } else {
-            $(this).find("textarea").addClass("past");
-        }
+    $(".hour-block").each(function () {
+        console.log($(this).attr("data-hours"));
+    
+            $(this).find('textarea').addClass("current");
         });
 }
 
-$(".b9").on("click", function() {
-    var entertext = $(".e9").val();
-    localStorage.setItem("9:00", entertext);
-});
-$(".e9").val(localStorage.getItem("9:00"));
+update();
+// each one of the button clicks are being used to save
+// the textare for each hour that has been saved with the button click
+// from 9am - 5pm
 
-$(".b10").on("click", function() {
-    var entertext = $(".e10").val();
-    localStorage.setItem("10:00", entertext);
-});
-$(".e10").val(localStorage.getItem("10:00"));
 
-$(".b11").on("click", function() {
-    var entertext = $(".e11").val();
-    localStorage.setItem("11:00", entertext);
-});
-$(".e11").val(localStorage.getItem("11:00"));
 
-$(".b12").on("click", function() {
-    var entertext = $(".e12").val();
-    localStorage.setItem("12:00", entertext);
-});
-$(".e12").val(localStorage.getItem("12:00"));
-
-$(".b1").on("click", function() {
-    var entertext = $(".e1").val();
-    localStorage.setItem("1:00", entertext);
-});
-$(".e1").val(localStorage.getItem("1:00"));
-
-$(".b2").on("click", function() {
-    var entertext = $(".e2").val();
-    localStorage.setItem("2:00", entertext);
-});
-$(".e2").val(localStorage.getItem("2:00"));
-
-$(".b3").on("click", function() {
-    var entertext = $(".e3").val();
-    localStorage.setItem("3:00", entertext);
-});
-$(".e3").val(localStorage.getItem("3:00"));
-
-$(".b4").on("click", function() {
-    var entertext = $(".e4").val();
-    localStorage.setItem("4:00", entertext);
-});
-$(".e4").val(localStorage.getItem("4:00"));
-
-$(".b5").on("click", function() {
-    var entertext = $(".e5").val();
-    localStorage.setItem("5:00", entertext);
-});
-$(".e5").val(localStorage.getItem("5:00"));
 
