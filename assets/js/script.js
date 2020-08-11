a4e33f5befcb5075f6811810006778a8646fd50e
DougCBrown <98bluecamaro@gmail.com>
2020-08-11 14:28:44 -0400

    added the save to localstorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
 });
 // here I am trying to build the function to fill the
 // background colors for the textarea future - green, current - red, past - gray
+// 
 function update() {
 
     var currentDate = new Date();
@@ -16,6 +17,7 @@ function update() {
         var thisHours = $(this).data('hours');
         var difference = currentDate.getHours() - thisHours;
     
+
         if(difference < 0) {
             $(this).find('textarea').addClass('future');
         } else if(difference === 0) {
@@ -23,9 +25,19 @@ function update() {
         } else {
             $(this).find('textarea').addClass('past');
         }
-      
+      $(this).find('textarea').val(localStorage.getItem(thisHours));
+ 
     });
 }
+
+$('.save-button').on('click', function () {
+    var textValue = $(this).siblings('textarea').val();
+    var parentHour = $(this).parent().data('hours');
+
+
+    localStorage.setItem(parentHour, textValue);
+})
+
 update();
 
 
